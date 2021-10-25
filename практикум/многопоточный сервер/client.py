4c5469d10fa012e4c9bb0e84584c093d5df87faf
Jek4 <90418243+Jek4@users.noreply.github.com>
2021-10-25 13:19:27 +0300

    Create client.py

---
+++ практикум/многопоточный сервер/client.py
@@ -0,0 +1,13 @@
+import socket
+from time import sleep
+sock = socket.socket()
+sock.setblocking(1)
+sock.connect(('192.168.1.39', 9090))
+while True:
+    msg = input()
+    sock.send(msg.encode())
+    if msg == 'exit':
+        break
+    data = sock.recv(1024)
+    print(f'accepted from server:$ {data.decode()}')
+sock.close()
